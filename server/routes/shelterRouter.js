f8d9769c69660583145c26b7ff0bde0039461a10
Joyce Oh <joyce.oh@live.com>
2024-08-04 18:02:02 -0400

    shelterrouter merge resolution

--- server/routes/shelterRouter.js
+++ server/routes/shelterRouter.js
@@ -1,24 +1,10 @@
 const express = require('express');
 
 const shelterController = require('../controllers/shelterController.js');
+const petController = require('../controllers/petController.js');
 const multer = require('multer');
 const path = require('path')
-const petController = require('../controllers/petController.js');
-
 const router = express.Router();
-
-//storage
-const Storage = multer.diskStorage({
-  destination:(req, file, cb) => {
-    cb(null, path.resolve(__dirname, '../models/images'))
-  },
-  filename:(req, file, cb) => {
-    cb(null, Date.now() + '--' + file.originalname);
-  }
-});
-const upload = multer({
-  storage: Storage
-})
 
 //storage
 const Storage = multer.diskStorage({
@@ -52,16 +38,5 @@ router.get('/login',
     return res.status(200).json(res.locals);
 });
 
-router.post('/addpet',
-  petController.createPet,
-  (req, res) => {
-    return res.status(200).json(res.locals);
-});
-
-router.delete('/deletepet',
-  petController.deletePet,
-  (req, res) => {
-    return res.status(200).json(res.locals);
-});
 
 module.exports = router;
