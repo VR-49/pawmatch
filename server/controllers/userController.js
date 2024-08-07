88ef7677302fade0806024068c3673ee8b25c977
Kenny <kennyjolson@gmail.com>
2024-08-07 10:21:22 -0500

    early

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,46 +3,43 @@ const fsCallback = require('fs');
 const path = require('path');
 const { Account, Pet, Human, Shelter } = require('../models/models.js');
 
-const userController = {}
+const userController = {};
 
 userController.login = (req, res, next) => {
   const { username, password } = req.body;
 
-  
   console.log('in usercontroller login');
   Account.find({ username })
-  .then(user => {
-    // console.log('found user', user[0]);
-    // console.log(password, user[0].password);
-    if (password === user[0].password) {
-      console.log('corect password');
-      res.locals.account = user;
-      res.locals.username = username;
-      res.locals.isOrg = user[0].isOrg;
-      return next();
-    }
-    else return next({message: 'incorrect username or password'});
-  })
-  .catch(err => {
-    return next({message: 'incorrect username'});
-  })
-}
+    .then((user) => {
+      // console.log('found user', user[0]);
+      // console.log(password, user[0].password);
+      if (password === user[0].password) {
+        console.log('corect password');
+        res.locals.account = user;
+        res.locals.username = username;
+        res.locals.isOrg = user[0].isOrg;
+        return next();
+      } else return next({ message: 'incorrect username or password' });
+    })
+    .catch((err) => {
+      return next({ message: 'incorrect username' });
+    });
+};
 
 userController.signup = (req, res, next) => {
-  console.log('inside of signup middleware: ', req.body)
+  console.log('inside of signup middleware: ', req.body);
   const { username, password, email, isOrg } = req.body;
 
   console.log('in usercontroller signup');
-  Account.create({username, password, email, isOrg})
-  .then((user) => {
-    res.locals.user = user;
-    console.log('user is', user);
-    return next();
-  })
-  .catch(err => {
-    return next(err);
-  }
-  )
-}
+  Account.create({ username, password, email, isOrg })
+    .then((user) => {
+      res.locals.user = user;
+      console.log('user is', user);
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
 
 module.exports = userController;
