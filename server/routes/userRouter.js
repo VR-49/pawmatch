6822c1e2c027238bb61f903c829ac22aaa0718a1
Kenny <kennyjolson@gmail.com>
2024-08-06 19:49:44 -0500

    gonna change

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,32 +8,35 @@ const userController = require('../controllers/userController.js');
 const shelterController = require('../controllers/shelterController.js');
 const humanController = require('../controllers/humanController.js');
 
+const cookieController = require('../cookies/cookieController');
+
 const router = express.Router();
 
 // const JWT_SECRET = process.env.JWT_SECRET || 'defaultsecretkey';
 router.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname,'../../src/index.html'))
+  res.status(200).sendFile(path.resolve(__dirname, '../../src/index.html'));
 });
 
 //generic login on landing page
-router.post('/signup', 
-    userController.signup,
-    (req, res) => {
-      return res.status(200).json(res.locals.body);
-      //inside the client side, after the fetch request we .then(data => if data.isOrg then fetch post shelter request else fetch post human request)
+router.post('/signup', userController.signup, (req, res) => {
+  return res.status(200).json(res.locals.body);
+  //inside the client side, after the fetch request we .then(data => if data.isOrg then fetch post shelter request else fetch post human request)
 });
 
-router.post('/login', 
-  userController.login, 
+router.post(
+  '/login',
+  userController.login,
   async (req, res, next) => {
-    if (res.locals.isOrg) { return shelterController.login(req, res, next); }
-    else { return humanController.login(req, res, next); }
+    if (res.locals.isOrg) {
+      return shelterController.login(req, res, next);
+    } else {
+      return humanController.login(req, res, next);
+    }
   },
   (req, res) => {
     console.log(res.locals);
     return res.status(200).json(res.locals);
   }
 );
-
 
 module.exports = router;
